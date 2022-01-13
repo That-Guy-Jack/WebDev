298260b7d02409a464e13d82f8b6266df781bd1a
Jack <Github@thatguyjack.co.uk>
2022-01-13 09:34:40 +0000

    fixed seconday DB address

--- Website/portfolios/index.php
+++ Website/portfolios/index.php
@@ -5,7 +5,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-$servername = "db.thatguyjack.co.uk";
+$servername = "192.168.5.16";
 $username = "webdev";
 $password = "webdev32145";
 $dbname = "webdev_db";
